b43c6c59e5a65c10aa63e2a4a178b91b34db0e3a
Michael Maloney <michael@maloney.at>
2020-11-30 10:34:55 -0700

    Change Term to be a container for a reference-counted TermNode.

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,21 @@ impl fmt::Debug for PrimFn {
 }
 
 #[derive(Clone, Debug)]
-enum Term {
+struct Term(rc::Rc<TermNode>);
+
+impl AsRef<TermNode> for Term {
+    fn as_ref(&self) -> &TermNode {
+        use std::borrow::Borrow;
+        let Term(rc_tn) = self;
+        rc_tn.borrow()
+    }
+}
+
+#[derive(Clone, Debug)]
+enum TermNode {
     Var(String),
-    Lam(String, Type, Box<Term>),
-    App(Box<Term>, Box<Term>),
+    Lam(String, Type, Term),
+    App(Term, Term),
     BoolLit(bool),
     NatLit(u64),
     Prim(PrimFn),
@@ -38,28 +49,29 @@ impl Context {
     }
 }
 
-fn eval(t: &Term, _ctx: &Context) -> Term {
-    use Term::*;
-    match t {
+fn eval(t: Term, _ctx: &Context) -> Term {
+    use TermNode::*;
+    let s = match t.as_ref() {
         Var(x) => Var(x.clone()),
         Lam(x, ty, body) => Lam(x.clone(), ty.clone(), body.clone()),
-        App(f, v) => match *f.clone() {
-            Lam(x, ty, body) => subst(&body, x.clone(), &v.clone()),
-            Prim(prim_fn) => eval_prim(prim_fn, *v.clone()),
+        App(f, v) => return match f.as_ref() {
+            Lam(x, ty, body) => subst(body.clone(), x.clone(), v.clone()),
+            Prim(prim_fn) => eval_prim(*prim_fn, v.clone()),
             _ => panic!("Applied argument to non-function."),
         },
         BoolLit(b) => BoolLit(*b),
         NatLit(n) => NatLit(*n),
         Prim(prim_fn) => Prim(prim_fn.clone()),
-    }
+    };
+    Term(rc::Rc::new(s))
 }
 
 fn eval_prim(prim_fn: PrimFn, v: Term) -> Term {
-    use Term::*;
+    use TermNode::*;
     match prim_fn {
         Succ => {
-            if let NatLit(n) = v {
-                NatLit(n + 1)
+            if let NatLit(n) = v.as_ref() {
+                Term(rc::Rc::new(NatLit(n + 1)))
             } else {
                 panic!("Can't succ on non-Nat.")
             }
@@ -67,12 +79,12 @@ fn eval_prim(prim_fn: PrimFn, v: Term) -> Term {
     }
 }
 
-fn subst(t: &Term, x: String, v: &Term) -> Term {
-    use Term::*;
-    match t {
+fn subst(t: Term, x: String, v: Term) -> Term {
+    use TermNode::*;
+    let s = match t.as_ref() {
         Var(y) => {
             if x == *y {
-                v.clone()
+                return v.clone()
             } else {
                 Var(y.clone())
             }
@@ -81,27 +93,26 @@ fn subst(t: &Term, x: String, v: &Term) -> Term {
             if x == *y {
                 Lam(x.clone(), ty.clone(), body.clone())
             } else {
-                Lam(y.to_string(), ty.clone(), Box::new(subst(body, x, v)))
+                Lam(y.to_string(), ty.clone(), subst(body.clone(), x, v))
             }
         }
         App(f, w) => App(
-            Box::new(subst(f, x.clone(), v)),
-            Box::new(subst(w, x.clone(), v)),
+            subst(f.clone(), x.clone(), v.clone()),
+            subst(w.clone(), x.clone(), v.clone()),
         ),
         BoolLit(b) => BoolLit(*b),
         NatLit(n) => NatLit(*n),
         Prim(f) => Prim(f.clone()),
-    }
+    };
+
+    Term(rc::Rc::new(s))
 }
 
 fn main() {
-    use Term::*;
-    fn addOneFn(t: &Term) -> Term {
-        match t {
-            NatLit(n) => NatLit(n + 1),
-            _ => panic!("No idea how to add 1 to this!"),
-        }
-    }
-    let term = App(Box::new(Prim(PrimFn::Succ)), Box::new(NatLit(5)));
-    dbg!(eval(&term, &Context::empty()));
+    use TermNode::*;
+    let term = Term(rc::Rc::new(App(
+        Term(rc::Rc::new(Prim(PrimFn::Succ))),
+        Term(rc::Rc::new(NatLit(5)))))
+    );
+    dbg!(eval(term, &Context::empty()));
 }
