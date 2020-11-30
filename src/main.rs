efe3580e236878e11767c21a013fc41a34ad3a77
Michael Maloney <michael@maloney.at>
2020-11-30 10:34:56 -0700

    Add snazzy quail logo.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
+    println!("{}", include_str!("../assets/quail.txt"));
     let mut runtime = eval::Runtime::load(filename);
     runtime.exec();
 }
